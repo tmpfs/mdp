e864c1e278b30d73e66ce8adb100437c2b72921c
cyberfunk <hello@cyberfunk.org>
2014-02-24 12:32:38 +0800

    Update readme.js comments.

--- readme/readme.js
+++ readme/readme.js
@@ -1,3 +1,10 @@
+/**
+ *  This a mock file to illustrate that documents
+ *  may be built from objects defined in modules as
+ *  well as json definitions.
+ *
+ *  This just defers to the package.json descriptor.
+ */
 module.exports = function() {
   return require('../package.json');
 }
