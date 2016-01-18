5cd60adea1f0cf0729c0302e008ff01421b16998
muji <freeformsystems@gmail.com>
2016-01-18 20:00:49 +0800

    Add lint files, initial test spec.

--- test/spec/mdp.js
+++ test/spec/mdp.js
@@ -1,9 +1,15 @@
-var path = require('path');
-var expect = require('chai').expect;
-var pkg = path.normalize(path.join(__dirname, '..', '..', 'package.json'));
+var path = require('path')
+  , expect = require('chai').expect
+  , mdp = require('../../lib/mdp')
+  , pkg = path.normalize(path.join(__dirname, '..', '..', 'package.json'));
 
 describe('mdp:', function() {
+
   it('should create program', function(done) {
+    var program = mdp(pkg);
+    expect(program).to.be.an('object');
+    //console.dir(program);
     done();
   });
+
 })
